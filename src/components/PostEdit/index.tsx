370cd2538a5c9304ca6efae4956a5feb0462e850
Hilverton Cesar <44450158+Hilverton@users.noreply.github.com>
2022-05-07 16:31:34 +0000

    Alteração no layout e adição de responsividade nas telas de edição

--- src/components/PostEdit/index.tsx
+++ src/components/PostEdit/index.tsx
@@ -1,11 +1,22 @@
 import { Edit, SimpleForm, ReferenceInput, TextInput, Labeled, SelectInput } from 'react-admin';
+import Grid from '@mui/material/Grid';
 
 const PostEdit = () => (
     <Edit title="Edição da postagem">
         <SimpleForm>
-            <ReferenceInput source="userId" reference="users"><SelectInput label="Usuário" disabled optionText="name" /></ReferenceInput>
-            <TextInput label="Título" source="title" resettable />
-            <TextInput label="Texto" multiline source="body" resettable />
+          <Grid container spacing={2} columns={{ xs: 1, sm: 8, md: 12 }}>
+            <Grid item xs={1} sm={4} md={6}>
+              <ReferenceInput source="userId" reference="users">
+                <SelectInput label="Usuário" fullWidth disabled optionText="name" />   
+              </ReferenceInput>
+            </Grid>
+            <Grid item xs={1} sm={4} md={6}>
+              <TextInput label="Título" fullWidth source="title" resettable />
+            </Grid>
+            <Grid item xs={1} sm={8} md={12}>
+              <TextInput label="Texto" fullWidth multiline source="body" resettable />
+            </Grid>
+          </Grid>  
         </SimpleForm>
     </Edit>
 );
--- src/components/UserEdit/index.tsx
+++ src/components/UserEdit/index.tsx
@@ -1,17 +1,40 @@
 import { Edit, SimpleForm, TextInput } from 'react-admin';
+import Grid from '@mui/material/Grid';
 
 const PostEdit = () => (
-    <Edit title="Edição do usuário">
-        <SimpleForm>
-          <TextInput label="Id" source="id" disabled />
-            <TextInput label="Nome" source="name" resettable />
-            <TextInput label="E-mail" source="email" resettable />
-            <TextInput label="Endereço/Rua" source="address.street" resettable />
-            <TextInput label="Telefone" source="phone" resettable />
-            <TextInput label="Site" source="website" resettable />
-            <TextInput label="Empresa" source="company.name" resettable />
-        </SimpleForm>
-    </Edit>
+  <Edit title="Edição do usuário">
+    <SimpleForm>
+      <Grid container spacing={2} columns={{ xs: 1, sm: 8, md: 12 }}>
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Identificador" fullWidth source="id" disabled />
+        </Grid>
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Nome" fullWidth source="name" resettable />
+        </Grid>
+
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="E-mail" fullWidth source="email" resettable />
+        </Grid>
+
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Endereço/Rua" fullWidth source="address.street" resettable />
+        </Grid>
+
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Telefone" fullWidth source="phone" resettable />
+        </Grid>
+
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Site" fullWidth source="website" resettable />
+        </Grid>
+
+        <Grid item xs={1} sm={4} md={4}>
+          <TextInput label="Empresa" fullWidth source="company.name" resettable />
+        </Grid>
+
+      </Grid>
+    </SimpleForm>
+  </Edit>
 );
 
 export default PostEdit
