e51bb099c33c76b44d095541770478f0c01226ba
Hilverton Cesar <44450158+Hilverton@users.noreply.github.com>
2022-05-10 23:35:30 +0000

    Versão mobile para listagem das postagens e usuários

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,15 +1,32 @@
-import { List, Datagrid, ReferenceField, TextField } from 'react-admin';
+import { List, SimpleList, Datagrid, ReferenceField, TextField } from 'react-admin';
+import { useMediaQuery } from '@mui/material';
 
-const PostList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <ReferenceField label="Escritor" source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField label="Id" source="id" />
-      <TextField label="Título" source="title" />
-    </Datagrid>
-  </List>
-);
+const PostList = () => {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  
+  return (
+    <List>
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.title}
+          secondaryText={record => record.body}
+          tertiaryText={record => <ReferenceField label="Escritor" source="userId"           
+              reference="users">
+            <TextField source="name" />
+          </ReferenceField>}
+        />    
+      ) : (
+        <Datagrid rowClick="edit">
+          <ReferenceField label="Escritor" source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField label="Id" source="id" />
+          <TextField label="Título" source="title" />
+          <TextField label="Texto" source="body" />
+        </Datagrid>
+      )}
+    </List>
+  )
+};
 
 export default PostList
--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,17 +1,30 @@
-import { List, Datagrid, EmailField, TextField } from 'react-admin';
+import { List, SimpleList, Datagrid, EmailField, TextField } from 'react-admin';
+import { useMediaQuery } from '@mui/material';
 
-const UserList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <TextField label="Id" source="id" />
-      <TextField label="Nome" source="name" />
-      <EmailField label="E-mail" source="email" />
-      <TextField label="Endereço/Rua" source="address.street" />
-      <TextField label="Telefone" source="phone" />
-      <TextField label="Site" source="website" />
-      <TextField label="Empresa" source="company.name" />
-    </Datagrid>
-  </List>
-);
+const UserList = () =>  {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  
+  return (
+    <List>
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.name}
+          secondaryText={record => record.email}
+          tertiaryText={record => record.username}
+        />    
+      ) : (
+        <Datagrid rowClick="edit">
+          <TextField label="Id" source="id" />
+          <TextField label="Nome" source="name" />
+          <EmailField label="E-mail" source="email" />
+          <TextField label="Endereço/Rua" source="address.street" />
+          <TextField label="Telefone" source="phone" />
+          <TextField label="Site" source="website" />
+          <TextField label="Empresa" source="company.name" />
+        </Datagrid>
+      )}
+    </List>
+  )
+};
 
 export default UserList
